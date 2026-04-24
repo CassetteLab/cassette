93e4425274adcb0cad59fc583f05e5a13aea1135
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-04-24 17:12:13 +0200

    Initial Commit

---
+++ Cassette/CassetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CassetteApp.swift
+//  Cassette
+//
+//  Created by Mathieu Dubart on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct CassetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CassetteTests/CassetteTests.swift
@@ -0,0 +1,18 @@
+//
+//  CassetteTests.swift
+//  CassetteTests
+//
+//  Created by Mathieu Dubart on 24/04/2026.
+//
+
+import Testing
+
+struct CassetteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
