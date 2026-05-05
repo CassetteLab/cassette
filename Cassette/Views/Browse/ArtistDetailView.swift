ffe688e43918bda827ec82dff02ef75d0e53b07b
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-05-05 11:01:14 +0200

    Revert "feat(library): add optional zoom transition params to ArtistDetailView"
    This reverts commit d753b972f7c7d73ee8750f3f2cd0649c73118383.

--- Cassette/Views/Browse/ArtistDetailView.swift
+++ Cassette/Views/Browse/ArtistDetailView.swift
@@ -9,17 +9,13 @@ import SwiftData
 
 struct ArtistDetailView: View {
     let artist: ArtistID3
-    private let zoomSourceId: String?
-    private let zoomNamespace: Namespace.ID?
 
     @Environment(\.appContainer) private var container
     @State private var viewModel: ArtistDetailViewModel?
     @Query private var artistFavoriteMatches: [FavoriteRecord]
 
-    init(artist: ArtistID3, zoomSourceId: String? = nil, zoomNamespace: Namespace.ID? = nil) {
+    init(artist: ArtistID3) {
         self.artist = artist
-        self.zoomSourceId = zoomSourceId
-        self.zoomNamespace = zoomNamespace
         let cid = "artist:\(artist.id)"
         _artistFavoriteMatches = Query(filter: #Predicate<FavoriteRecord> { $0.id == cid })
     }
@@ -67,7 +63,6 @@ struct ArtistDetailView: View {
             if viewModel == nil { viewModel = ArtistDetailViewModel(artistId: artist.id, libraryService: svc) }
             await viewModel?.load()
         }
-        .cassetteZoomTransition(sourceID: zoomSourceId, in: zoomNamespace)
     }
 
     private var skeletonGrid: some View {
