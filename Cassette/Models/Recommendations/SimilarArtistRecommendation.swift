8469a56409aaef51f88050125872878cfb624228
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-05-14 22:05:02 +0200

    feat(models): add mbid field and nonisolated to SimilarArtistRecommendation

--- Cassette/Models/Recommendations/SimilarArtistRecommendation.swift
+++ Cassette/Models/Recommendations/SimilarArtistRecommendation.swift
@@ -5,7 +5,7 @@
 
 import Foundation
 
-struct SimilarArtistRecommendation: Sendable, Identifiable, Equatable {
+nonisolated struct SimilarArtistRecommendation: Sendable, Identifiable, Equatable {
     let id: String
     let name: String
     let coverArt: String?
