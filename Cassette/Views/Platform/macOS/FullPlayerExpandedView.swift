3dc9ff331973c00d811efd9876fa60973ef90e84
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-05-22 22:26:48 +0200

    fix(macos): push player overlay buttons into titlebar zone

--- Cassette/Views/Platform/macOS/FullPlayerExpandedView.swift
+++ Cassette/Views/Platform/macOS/FullPlayerExpandedView.swift
@@ -150,7 +150,7 @@ struct FullPlayerExpandedView: View {
             .clipShape(Capsule())
         }
         .padding(.horizontal, 20)
-        .padding(.top, 0)
+        .padding(.top, -16)
     }
 
     private func wideLayout(_ geo: GeometryProxy) -> some View {
