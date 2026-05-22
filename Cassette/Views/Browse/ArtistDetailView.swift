ac2f602158e3241713b190c6f6b94b6ad6cad2c3
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-05-22 23:50:28 +0200

    refactor: extract SimilarArtistCell to DesignSystem/Components

--- Cassette/Views/Browse/ArtistDetailView.swift
+++ Cassette/Views/Browse/ArtistDetailView.swift
@@ -252,47 +252,6 @@ struct ArtistDetailView: View {
     }
 }
 
-// MARK: - Similar artist cell
-
-private struct SimilarArtistCell: View {
-    let recommendation: SimilarArtistRecommendation
-    let externalImageURL: URL?
-    let onOutOfLibraryTap: () -> Void
-
-    var body: some View {
-        if recommendation.inLibrary {
-            cellContent
-        } else {
-            Button(action: onOutOfLibraryTap) {
-                cellContent
-            }
-            .buttonStyle(.plain)
-        }
-    }
-
-    private var cellContent: some View {
-        VStack(spacing: CassetteSpacing.xs) {
-            if recommendation.inLibrary, let coverArt = recommendation.coverArt {
-                CoverArtView(id: coverArt, size: 128, placeholderSystemImage: "person.fill")
-                    .frame(width: 64, height: 64)
-                    .clipShape(Circle())
-            } else {
-                ExternalCoverView(url: externalImageURL) {
-                    ArtistPlaceholderView(name: recommendation.name, size: 64)
-                }
-                .frame(width: 64, height: 64)
-                .clipShape(Circle())
-            }
-
-            Text(recommendation.name)
-                .font(.cassetteCaption)
-                .lineLimit(2)
-                .multilineTextAlignment(.center)
-                .frame(maxWidth: .infinity)
-        }
-    }
-}
-
 // MARK: - Out-of-library artist sheet
 
 struct OutOfLibraryArtistSheet: View {
