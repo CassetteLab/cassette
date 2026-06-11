133a136f95efe3f8a8d44e2c99f50260f236a0a7
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-06-11 21:14:57 +0200

    fix(ui): pad track list so mini player doesn't cover last tracks

--- Cassette/Views/Browse/PlaylistDetailView.swift
+++ Cassette/Views/Browse/PlaylistDetailView.swift
@@ -173,9 +173,7 @@ struct PlaylistDetailView: View {
         .environment(\.editMode, .constant(isEditing ? .active : .inactive))
         #endif
         .scrollContentBackground(.hidden)
-        #if os(iOS)
-        .contentMargins(.bottom, 80, for: .scrollContent)
-        #endif
+        .miniPlayerBottomMargin()
         .refreshable { await viewModel?.load() }
         .alert("Remove downloaded playlist?", isPresented: $showDeleteAlert) {
             Button("Remove", role: .destructive) { Task { await viewModel?.deleteDownload() } }
